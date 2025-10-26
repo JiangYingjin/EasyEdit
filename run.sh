a1df83464fd2342edaeb643d0c446e3756fafd0b
JiangYingjin <i@JYJ.cx>
2025-10-26 19:50:35 +0800

    Update run.sh to change conda environment and append output mode

--- run.sh
+++ run.sh
@@ -6,8 +6,10 @@
 #SBATCH --mem=128G
 #SBATCH -o slurm.log
 #SBATCH -e slurm.log
+#SBATCH --open-mode=append
 #SBATCH --time=00:15:00
 
 . ~/.zshrc
-conda activate easyedit
+# conda activate easyedit
+conda activate ke
 python run.py
